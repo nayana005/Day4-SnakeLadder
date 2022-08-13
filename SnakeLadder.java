6353501c194f36a3e77a2d971b5eb9fe2e3a0eb8
nayana005 <nayanasuresh1005@gmail.com>
2022-08-13 20:41:17 +0530

    Added: At position 0

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bl.snake_ladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("Welcome to the game of Snake and ladder ");
+		
+		System.out.println("Your current position is :" + position);
+
+}
+}
