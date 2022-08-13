59da280fc04e04b3251c4dadb8ff52c3dc64cda1
nayana005 <nayanasuresh1005@gmail.com>
2022-08-13 20:44:57 +0530

    Added: Rolling Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,10 +2,17 @@ package com.bl.snake_ladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-		int position = 0;
+		
 		System.out.println("Welcome to the game of Snake and ladder ");
 		
-		System.out.println("Your current position is :" + position);
+		int position = 0;
+		final int DICE_START_RANGE = 1;
+		final int DICE_END_RANGE = 6 - DICE_START_RANGE;
 
-}
+		System.out.println("Your current position is :" + position);
+		
+		int diceNumber = (int) Math.floor(Math.random() * DICE_END_RANGE) + DICE_START_RANGE;
+		System.out.println("You have got a Dice number of : "+ diceNumber);
+
+	}
 }
